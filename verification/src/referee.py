944b6d61caa9930f5459b94d3440cf00fb7d8e33
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-27 09:54:36 +0300

    validator rrename

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,50 +1,50 @@
 from checkio_referee import RefereeBase
 from checkio_referee.covercode import py_tuple
-from checkio_referee.verifications import BaseVerification, VerificationError
+from checkio_referee.validators import BaseValidator, ValidationError
 
 import settings
 import settings_env
 from tests import TESTS
 
 
-class SwapSortVerification(BaseVerification):
+class SwapSortValidator(BaseValidator):
     @staticmethod
     def swap(array, i, j):
         array[i], array[j] = array[j], array[i]
 
-    def verify(self, result):
+    def validate(self, outer_result):
         in_data = self._test.get("input", [])
         array = list(in_data[:])
         la = len(array)
-        if not isinstance(result, str):
+        if not isinstance(outer_result, str):
             self.additional_data = "The result should be a string"
-            raise VerificationError
-        actions = result.split(",") if result else []
+            raise ValidationError
+        actions = outer_result.split(",") if outer_result else []
         for act in actions:
             if len(act) != 2 or not act.isdigit():
                 self.additional_data = "The wrong action: {}".format(act)
-                raise VerificationError
+                raise ValidationError
             i, j = int(act[0]), int(act[1])
             if i >= la or j >= la:
                 self.additional_data = "Index error: {}".format(act)
-                raise VerificationError
+                raise ValidationError
             if abs(i - j) != 1:
                 self.additional_data = "The wrong action: {}".format(act)
-                raise VerificationError
+                raise ValidationError
             self.swap(array, i, j)
         if len(actions) > (la * (la - 1)) // 2:
             self.additional_data = "Too many actions."
-            raise VerificationError
+            raise ValidationError
         if array != sorted(in_data):
             self.additional_data = "The array is not sorted."
-            raise VerificationError
+            raise ValidationError
 
 
 class Referee(RefereeBase):
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
-    VERIFICATION = SwapSortVerification
+    VALIDATOR = SwapSortValidator
     FUNCTION_NAME = "swap_sort"
     ENV_COVERCODE = {
         "python_3": py_tuple,
